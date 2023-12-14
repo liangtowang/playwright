b916275e8bda423ace430f8bfd8df71255ac4fa2
WANG Liang <liang.wang@soprasteria.com>
2023-12-14 10:42:04 +0100

    Fix the failed search, verify that the user is addedd successfully before it is searchable

--- tests/demo.spec.ts
+++ tests/demo.spec.ts
@@ -1,12 +1,17 @@
 import { test, expect } from '@playwright/test';
 import exp from 'constants';
 
+const userName = "Admin"
+const password = "admin123"
+const firstName = "Liang";
+const lastName = "Wang";
+
 test.beforeEach(async ({ page }) => {
   // Runs before each test and signs in each page.
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
   // Login with username and password
-  await page.getByPlaceholder('Username').fill('Admin');
-  await page.getByPlaceholder('Password').fill('admin123');
+  await page.getByPlaceholder('Username').fill(userName);
+  await page.getByPlaceholder('Password').fill(password);
   await page.getByRole('button', { name: 'Login' }).click();
 });
 
@@ -15,9 +20,6 @@ test('test add employee', async ({ page }) => {
 
   await page.getByRole('link', { name: 'PIM' }).click();
   await page.getByRole('link', { name: 'Add Employee' }).click();
-  
-  const firstName = "Liang";
-  const lastName = "Wang";
 
   await page.getByPlaceholder('First Name').fill(firstName);
   await page.getByPlaceholder('Last Name').fill(lastName);
@@ -32,6 +34,9 @@ test('test add employee', async ({ page }) => {
 
   // Save
   await page.getByRole('button', { name: 'Save' }).click();
+
+  // Verify it is saved successfully
+  await expect(page.getByText('Success', { exact: true})).toBeVisible();
 });
 
 test('test search employee', async ({ page }) => {
@@ -40,12 +45,12 @@ test('test search employee', async ({ page }) => {
   await page.getByRole('link', { name: 'PIM' }).click();
   await page.getByRole('link', { name: 'Employee List' }).click();
 
-  await page.getByPlaceholder('Type for hints...').first().fill('liang');
+  await page.getByPlaceholder('Type for hints...').first().fill(firstName);
   await page.getByRole('button', { name: 'Search' }).click();
   
-  await page.getByRole('row', { name: 'Liang Wang' }).first().click();
+  await page.getByRole('row', { name: firstName }).first().click();
 
-  await expect(page.getByRole('heading', { name: 'Liang Wang' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: firstName })).toBeVisible();
   
   // await expect(page.getByPlaceholder('First Name')).toHaveText('Liang');
   // await expect(page.getByPlaceholder('Last Name')).toHaveText('Wang');
