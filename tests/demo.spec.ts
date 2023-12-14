7b812c14c238f144165a9d38602ba6f2f04768ef
WANG Liang <liang.wang@soprasteria.com>
2023-12-14 13:15:48 +0100

    Remove .only added during debugging

--- tests/demo.spec.ts
+++ tests/demo.spec.ts
@@ -12,7 +12,7 @@ test.beforeEach(async ({ page }) => {
   await page.getByRole('button', { name: 'Login' }).click();
 });
 
-test.only('test add employee', async ({ page }) => {
+test('test add employee', async ({ page }) => {
   await page.goto(loginData.url.dashboardURL);
 
   await page.getByRole('link', { name: 'PIM' }).click();
