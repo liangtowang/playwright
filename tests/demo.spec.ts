e09420d2e0e55069cbe004021402c4a0e3131d73
WANG Liang <liang.wang@soprasteria.com>
2023-12-14 10:29:58 +0100

    Add test search employee

--- tests/demo.spec.ts
+++ tests/demo.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import exp from 'constants';
 
 test.beforeEach(async ({ page }) => {
   // Runs before each test and signs in each page.
@@ -8,7 +9,6 @@ test.beforeEach(async ({ page }) => {
   await page.getByPlaceholder('Password').fill('admin123');
   await page.getByRole('button', { name: 'Login' }).click();
 });
-
 
 test('test add employee', async ({ page }) => {
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
@@ -32,4 +32,27 @@ test('test add employee', async ({ page }) => {
 
   // Save
   await page.getByRole('button', { name: 'Save' }).click();
+});
+
+test('test search employee', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
+
+  await page.getByRole('link', { name: 'PIM' }).click();
+  await page.getByRole('link', { name: 'Employee List' }).click();
+
+  await page.getByPlaceholder('Type for hints...').first().fill('liang');
+  await page.getByRole('button', { name: 'Search' }).click();
+  
+  await page.getByRole('row', { name: 'Liang Wang' }).first().click();
+
+  await expect(page.getByRole('heading', { name: 'Liang Wang' })).toBeVisible();
+  
+  // await expect(page.getByPlaceholder('First Name')).toHaveText('Liang');
+  // await expect(page.getByPlaceholder('Last Name')).toHaveText('Wang');
+  
+});
+
+test.afterAll('Teardown', async () => {
+  console.log('Done with tests');
+  // ...
 });
