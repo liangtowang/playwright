8d8b2a466c7e7485056836a867f1cd0703ab708b
WANG Liang <liang.wang@soprasteria.com>
2023-12-14 12:15:48 +0100

    Major refactoring of test data by putting it in JSON files

--- tests/demo.spec.ts
+++ tests/demo.spec.ts
@@ -1,28 +1,26 @@
 import { test, expect } from '@playwright/test';
 import exp from 'constants';
 
-const userName = "Admin"
-const password = "admin123"
-const firstName = "Liang";
-const lastName = "Wang";
+const loginData = JSON.parse(JSON.stringify(require('../test-data/LoginData.json')));
+const testData = JSON.parse(JSON.stringify(require('../test-data/TestData.json')));
 
 test.beforeEach(async ({ page }) => {
   // Runs before each test and signs in each page.
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.goto(loginData.url.baseURL);
   // Login with username and password
-  await page.getByPlaceholder('Username').fill(userName);
-  await page.getByPlaceholder('Password').fill(password);
+  await page.getByPlaceholder('Username').fill(loginData.admin.username);
+  await page.getByPlaceholder('Password').fill(loginData.admin.password);
   await page.getByRole('button', { name: 'Login' }).click();
 });
 
 test('test add employee', async ({ page }) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
+  await page.goto(loginData.url.dashboardURL);
 
   await page.getByRole('link', { name: 'PIM' }).click();
   await page.getByRole('link', { name: 'Add Employee' }).click();
 
-  await page.getByPlaceholder('First Name').fill(firstName);
-  await page.getByPlaceholder('Last Name').fill(lastName);
+  await page.getByPlaceholder('First Name').fill(testData.user.firstName);
+  await page.getByPlaceholder('Last Name').fill(testData.user.lastName);
 
   // Add image 
   // Start waiting for file chooser before clicking. 
@@ -30,7 +28,7 @@ test('test add employee', async ({ page }) => {
   await page.locator('form').getByRole('img', { name: 'profile picture' }).click();
 
   const fileChooser = await fileChooserPromise;
-  await fileChooser.setFiles('./tests/fixtures/imageName.png');
+  await fileChooser.setFiles('./test-data/fixtures/imageName.png');
 
   // Save
   await page.getByRole('button', { name: 'Save' }).click();
@@ -40,24 +38,20 @@ test('test add employee', async ({ page }) => {
 });
 
 test('test search employee', async ({ page }) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
+  await page.goto(loginData.url.dashboardURL);
 
   await page.getByRole('link', { name: 'PIM' }).click();
   await page.getByRole('link', { name: 'Employee List' }).click();
 
-  await page.getByPlaceholder('Type for hints...').first().fill(firstName);
+  await page.getByPlaceholder('Type for hints...').first().fill(testData.user.firstName);
   await page.getByRole('button', { name: 'Search' }).click();
   
-  await page.getByRole('row', { name: firstName }).first().click();
+  await page.getByRole('row', { name: testData.user.firstName }).first().click();
 
-  await expect(page.getByRole('heading', { name: firstName })).toBeVisible();
-  
-  // await expect(page.getByPlaceholder('First Name')).toHaveText('Liang');
-  // await expect(page.getByPlaceholder('Last Name')).toHaveText('Wang');
-  
+  await expect(page.getByRole('heading', { name: testData.user.firstName })).toBeVisible(); 
 });
 
-test.afterAll('Teardown', async () => {
+test.afterAll('Teardown', async ({ page }) => {
   console.log('Done with tests');
-  // ...
+  await page.close();
 });
